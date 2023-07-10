9d9450e8efa3f5283c54ea2298a79b988c70d5f0
Your Name <you@example.com>
2023-07-10 17:37:02 +0900

    7.10 for

---
+++ for_1.py
@@ -0,0 +1,15 @@
+# words = "I will finish at 5:50"
+# for i in words:
+#     print(i)
+
+# data_list = ["a", "b", "c"]
+# for i in data_list:
+#     print(i)
+
+# data_list = ["a", "b", "c"]
+# for i in data_list:
+#     print("%s 헐" %i)
+
+data_list = ["a", "b", "c"]
+for i in data_list:
+    print(f"{i} 헐")
---
+++ for_enumerate.py
@@ -0,0 +1,18 @@
+# for entry in enumerate(['A', 'B', 'C']):
+#     print(entry)
+
+# for i , data in enumerate(['A', 'B', 'C']):
+#     print(i)
+#     print(data)
+#     print('------------------------------')
+    
+# for _ , data in enumerate(['A', 'B', 'C']):    
+#     print(data)
+#     print('==============================')
+
+for i , data in enumerate(['A', 'B', 'C']):
+    print(f"{i}번째는 {data}입니다")
+
+    print("{} 번째는 {} 입니다.".format(i, data))
+    # print(data)
+    # print('------------------------------')
